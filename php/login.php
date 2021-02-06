d9b46b7095d0b42f39628963c0a5887bb3856be8
Josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-02-06 18:00:18 -0500

    login

--- php/login.php
+++ php/login.php
@@ -14,7 +14,6 @@ require_once "../php/config.php";
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<title>Sign In</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-		<script type="text/javascript" src="../js/gripe.js"></script>
 		<style type="text/css">
 			body{
 				font: 18px sans-serif;
