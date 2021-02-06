2d968c461c38cb344974d0cabb39a6ee03f5d80c
Josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-02-06 18:01:19 -0500

    added my insert file to the database table

---
+++ php/gripeinsert.php
@@ -0,0 +1,19 @@
+<?php
+	//gripeinsert.php
+
+	include('config.php');
+
+	session_start();
+
+	$username = ($_SESSION['username']);
+	$profileimage = ($_POST['profileimage']);
+	$gripe = ($_POST['gripe']);
+	$gripe = str_replace("'","''", $gripe);
+
+	$sql = ("INSERT INTO messages (username, profileimage, gripe) VALUES ('$username', '$profileimage', '$gripe' )");
+	if($link->query($sql) === true){
+	  $_SESSION['username'] = $username;
+	} else{
+		echo "ERROR: Not able to execute $sql. " . $link->error;
+	}
+?>
