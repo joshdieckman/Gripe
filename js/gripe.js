ae9aff7356dcba143f096e3e236853e06f187085
Josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-02-06 17:56:04 -0500

    gripe.js

--- js/gripe.js
+++ js/gripe.js
@@ -1,40 +1,12 @@
-const emailInput = document.getElementById("email");
-const passwordInput = document.getElementById("password");
+function ajaxpost() {
+	var data = new FormData();
+	data.append("username", document.getElementById("username").value);
+	data.append("profileimage", document.getElementById("profileimage").value);
+	data.append("gripe", document.getElementById("gripe").value);
 
-/*Validators*/
-
-// Must contain a lowercase, uppercase letter and a number
-
-// Must be a valid email address
-function isValidEmail(email) {
-  return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "gripeinsert.php");
+	xhr.onload = function () {console.log(this.response); };
+	xhr.send(data);
+	return false;
 }
-
-function isValidPassword(password) {
-  return /[a-z]/.test(password) && /[A-Z]/.test(password) && /[0-9]/.test(password);
-}
-
-/* Set up Events*/
-
-function showOrHideTip(show, element) {
-  // show element when show is true, hide when false
-  if (show) {
-    element.style.display = "inherit";
-  } else {
-    element.style.display = "none";
-  }
-}
-
-function createListener(validator) {
-  return e => {
-    const text = e.target.value;
-    const valid = validator(text);
-    const showTip = text !== "" && !valid;
-    const tooltip = e.target.nextElementSibling;
-    showOrHideTip(showTip, tooltip);
-  };
-}
-
-emailInput.addEventListener("input", createListener(isValidEmail));
-
-passwordInput.addEventListener("input", createListener(isValidPassword));
