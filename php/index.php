76c2bea2ba450c6bd96b308c5d8ba9759bfbce09
Josh.dieckman@gmail.com <josh.dieckman@gmail.com>
2021-03-02 16:32:42 -0500

    display timestamp on the messages

--- php/index.php
+++ php/index.php
@@ -374,6 +374,7 @@
 							echo '<div class="message">
 								<img class="image" src="/images/' . $row["profileimage"] . '" height="100px" width="100px">
 								<div class="words">
+                                    <p>' . $row["timestamp"] . '</p>
 									<h4>' . $row["username"] . '</h4>
 									<p>' . $row["gripe"] . '</p>
 								</div>
